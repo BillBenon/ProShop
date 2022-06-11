f128f664509dc58c6170fc52993ac2df63766c1c
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-06-11 10:00:00 +0100

    fix: product details screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 import Rating from "../components/Rating";
 import products from "../products";
 
-function ProductScreen({ match }) {
-  const product = products.find((p) => p._id == match.params.id);
+function ProductScreen() {
+  const productId = useParams().id;
+  const product = products.find((p) => p._id == productId);
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
