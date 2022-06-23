2b762b99494280ba4118995799becde0a2b0c412
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-06-23 15:41:43 +0200

    product details reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,10 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-import { productListReducers } from "./reducers/productReducers";
+import {
+  productListReducers,
+  productDetailsReducers,
+} from "./reducers/productReducers";
 
 const reducer = combineReducers({
   productList: productListReducers,
+  productDetails: productDetailsReducers,
 });
 
 const initialState = {};
