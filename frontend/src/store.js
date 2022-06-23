1cc42c27ef6df700d97a870f0b58139215ccb0b0
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-06-23 11:24:56 +0200

    fix: reducer should be an object

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,7 +10,7 @@ const initialState = {};
 const middleware = [thunk];
 
 const store = configureStore(
-  reducer,
+  { reducer },
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
