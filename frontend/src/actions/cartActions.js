f59cea5a2264af1b09398cf61a333a6c6fd53c88
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-07-06 20:46:58 +0200

    feat: backend register user

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+import { BACKEND_URL } from "../config";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`${BACKEND_URL}/api/products/${id}`);
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+
+  localStorage.removeItem(
+    "cartItems",
+    JSON.stringify(getState().cart.cartItems)
+  );
+};
